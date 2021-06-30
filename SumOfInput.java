447f2533ec9e131f83c50a955a8e628cbb26f331
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-06-29 22:05:21 -0400

    all tests pass

--- SumOfInput.java
+++ SumOfInput.java
@@ -1,26 +1,32 @@
 /**
- * Write a description of class SumOfInput here.
+ * Returning the sum of numbers 1 to n.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Bobbi
+ * @version 06/29/21
  */
 public class SumOfInput
 {
     // instance variables - replace the example below with your own
-    private Integer sum;
+    private int oneToNumber;
+    
 
     /**
      * Constructor for objects of class SumOfInput
      */
     public SumOfInput()
     {
-        // initialise instance variables
-        sum = 0;
+        //initialise instance variables
+        Integer oneToNumber;
     }
     
-    public Integer oneToNumber(Integer numberToSum){
-
-        return sum;
+    public Integer oneToNumber(int n){
+       int sum = 0;
+        
+        for (int x = 1; x <= n; x++){
+            sum += x;
+        }
+        
+            return sum;
     }
 
 }
